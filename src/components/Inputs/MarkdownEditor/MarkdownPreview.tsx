b2781b39dd8f6cea8f9b5434216e1abb7d6ea00c
Matt Pitcher <matt.e.pitcher@gmail.com>
2019-05-05 14:29:16 -0600

    preview seems to be fully functional now

--- src/components/Inputs/MarkdownEditor/MarkdownPreview.tsx
+++ src/components/Inputs/MarkdownEditor/MarkdownPreview.tsx
@@ -1,41 +1,12 @@
 import * as React from "react";
-import styled, { StyledComponentClass } from "styled-components";
+import { Converter } from "showdown";
+import styled from "styled-components";
 //import { theme } from "../../../theming/theme";
 
 const PreviewWrapper = styled.div`
     width: 100%;
     height: 100%;
 `;
-
-const H1 = styled.h1`
-
-`;
-
-const H2 = styled.h2`
-
-`;
-
-const UnstyledText = styled.span``;
-
-const Italic = styled.span`
-    font-style: italic;
-`;
-
-const Bold = styled.span`
-    font-weight: bold;
-`;
-
-const BoldItalic = styled.span`
-    font-style: italic;
-    font-weight: bold;
-`;
-
-interface IMarkdownElement {
-    Element: StyledComponentClass<React.DetailedHTMLProps<React.HTMLAttributes<any>, any>, any, React.DetailedHTMLProps<React.HTMLAttributes<any>, any>>;
-    Children?: IMarkdownElement[];
-    Text: string;
-    TextAfter?: string;
-}
 
 interface IMarkdownPreviewProps {
     Value: string;
@@ -51,123 +22,36 @@ export class MarkdownPreview extends React.PureComponent<IMarkdownPreviewProps> 
     }
 
     private buildPreview = (): JSX.Element => {
-        const elements: JSX.Element[] = [];
-        const lines: string[] = this.props.Value.split(/\n/g);
-        lines.map((line) => {
-            elements.push(this.buildLine(line));
-            elements.push(<br />);
+        const converter = new Converter({
+            omitExtraWLInCodeBlocks: false, /* default = false */
+            noHeaderId: true, /* default = false */
+            customizedHeaderId: false, /* default = false */
+            ghCompatibleHeaderId: false, /* default = false */
+            prefixHeaderId: false, /* default = false, can be string to set ID */
+            parseImgDimensions: false, /* default = false */
+            headerLevelStart: 1, /* default = 1 */
+            simplifiedAutoLink: false, /* default = false */
+            excludeTrailingPunctuationFromURLs: false, /* default = false */
+            literalMidWordUnderscores: false, /* default = false */
+            strikethrough: true, /* default = false */
+            tables: true, /* default = false */
+            tablesHeaderId: false, /* default = false */
+            ghCodeBlocks: true, /* default = true */
+            tasklists: false, /* default = false */
+            smoothLivePreview: false, /* default = false */
+            smartIndentationFix: false, /* default = false */
+            disableForced4SpacesIndentedSublists: false, /* default = false */
+            simpleLineBreaks: false, /* default = false */
+            requireSpaceBeforeHeadingText: false, /* default = false */
+            openLinksInNewWindow: true, /* default = false */
+            backslashEscapesHTMLTags: false, /* default = false */
+            emoji: true, /* default = false */
+            underline: true, /* default = false */
+            completeHTMLDocument: false, /* default = false */
+            metadata: false, /* default = false */
+            splitAdjacentBlockquotes: false, /* default = false */
         });
-        return <PreviewWrapper>{elements}</PreviewWrapper>;
-    }
-
-    private buildLine = (line: string): JSX.Element => {
-        const mdElement: IMarkdownElement = this.findAndApplyHeader(line);
-        line = mdElement.Text;
-        if (mdElement.Element) {
-            mdElement.Children = [{Element: UnstyledText, Text: line.substring(0, line.indexOf("*") === -1 ? line.length : line.indexOf("*"))}];
-        } else {
-            mdElement.Element = UnstyledText;
-            mdElement.Children = [];
-            mdElement.Text = line.substring(0, line.indexOf("*") === -1 ? line.length : line.indexOf("*"));
-        }
-        const boldItalics: IMarkdownElement = this.findAndApplyBoldItalic(line.substring(line.indexOf("*"), line.length), []);
-        if (boldItalics) {
-            mdElement.Children = mdElement.Children.concat([boldItalics]).concat(boldItalics.Children);
-        }
-        let element = <span>{mdElement.Text}</span>;
-        if (mdElement.Element !== undefined)
-        {
-            const children: any[] = [];
-            if (mdElement.Children) {
-                mdElement.Children.filter((val) => val.Element !== undefined).map((child) => { children.push(<span><child.Element>{child.Text}</child.Element>{child.TextAfter}</span>); });
-            }
-            element = React.createElement(mdElement.Element, undefined, children);
-        }
-        return element;
-    }
-
-    private findAndApplyHeader = (line: string): IMarkdownElement => {
-        const markdownElement: IMarkdownElement = { Element: undefined, Text: line };
-        if (line[0] === "#") {
-            const rank: number = line.split(" ")[0].length;
-            switch (rank) {
-                case 1:
-                    markdownElement.Element = H1;
-                    markdownElement.Text = line.substring(rank, line.length);
-                    break;
-                case 2:
-                    markdownElement.Element = H2;
-                    markdownElement.Text = line.substring(rank, line.length);
-                    break;
-                default:
-                    markdownElement.Element = H2;
-                    markdownElement.Text = line.substring(rank, line.length);
-                    break;
-            }
-        }
-        return markdownElement;
-    }
-
-    // DUPLICATE FUNCTIOn -- is also in MarkdownEditor.tsx
-    private repeatString = (value: string, times: number): string => {
-        let repeatedValue: string = "";
-        while (times > 0) {
-            repeatedValue += value;
-            times--;
-        }
-        return repeatedValue;
-    }
-
-    private getFirstCharRepeatCount = (value: string): number => {
-        let count: number = 0;
-        if (value.length > 0) {
-            const char: string = value[0];
-            while(value[count] === char) {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    private findAndApplyBoldItalic = (line: string, children: IMarkdownElement[]): IMarkdownElement => {
-        const markdownElement: IMarkdownElement = { Element: undefined, Children: children, Text: line };
-        const first: number = line.indexOf("*");
-        const last: number = line.lastIndexOf("*");
-        if (first !== -1 && first !== last) {
-            let repeatCount: number = this.getFirstCharRepeatCount(line.substring(first, line.length));
-            let next: number = line.indexOf(this.repeatString("*", repeatCount), first + repeatCount);
-            if (next === -1) {
-                while (next === -1 && repeatCount > 1) {
-                    repeatCount--;
-                    next = line.indexOf(this.repeatString("*", repeatCount), first + repeatCount);
-                }
-            }
-            while (next !== -1) {
-                console.log(repeatCount);
-                switch (repeatCount) {
-                    case 0:
-                        return undefined;
-                    case 1:
-                        markdownElement.Element = Italic;
-                        break;
-                    case 2:
-                        markdownElement.Element = Bold;
-                        break;
-                    default:
-                        markdownElement.Element = BoldItalic;
-                        break;
-
-                }
-                markdownElement.Text = `${line.substring(first + repeatCount, next)}`;
-                markdownElement.TextAfter = `${line.substring(next + repeatCount, line.indexOf("*", next + repeatCount) === -1 ? next + repeatCount : line.indexOf("*", next + repeatCount))}`;
-                line = line.substring(next + repeatCount, line.length);
-                children.unshift(this.findAndApplyBoldItalic(line, children));
-                next = -1;
-                repeatCount = this.getFirstCharRepeatCount(line.substring(first, line.length));
-            }
-        }
-        markdownElement.Children = children;
-        markdownElement.TextAfter = line;
-        return markdownElement;
+        const html = converter.makeHtml(this.props.Value);
+        return <PreviewWrapper dangerouslySetInnerHTML={{__html: html}}/>;
     }
 }
