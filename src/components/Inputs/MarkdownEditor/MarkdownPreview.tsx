470d150fc8a2a81517e244ab865f203c541473f5
Matt Pitcher <matt.e.pitcher@gmail.com>
2019-05-05 00:10:54 -0600

    added preivew, fixed interface names

--- src/components/Inputs/MarkdownEditor/MarkdownPreview.tsx
+++ src/components/Inputs/MarkdownEditor/MarkdownPreview.tsx
@@ -49,18 +49,18 @@ interface ISelectionIndices {
     End: number;
 }
 
-interface IMarkdownEditorProps {
+interface IMarkdownPreviewProps {
     Value?: string;
     Placeholder?: string;
 }
 
-interface IMarkdownEditorState {
+interface IMarkdownPreviewState {
     Value: string;
     Selected: ISelectionIndices;
 }
 
-export class MarkdownEditor extends React.Component<IMarkdownEditorProps, IMarkdownEditorState> {
-    public constructor(props: IMarkdownEditorProps) {
+export class MarkdownPreview extends React.Component<IMarkdownPreviewProps, IMarkdownPreviewState> {
+    public constructor(props: IMarkdownPreviewProps) {
         super(props);
         this.state = {
             Value: this.props.Value || "",
@@ -119,95 +119,6 @@ export class MarkdownEditor extends React.Component<IMarkdownEditorProps, IMarkd
                     onSelect={this.handleSelect}
                 />
             </MDEWrapper>
-        );
-    }
-
-    private handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-        this.setState({Value: event.target.value});
-    }
-
-    private handleSelect = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-        const textArea: HTMLTextAreaElement = event.target;
-        this.setState({ Selected: { Start: textArea.selectionStart, End: textArea.selectionEnd }});
-    }
-
-    private repeatString = (value: string, times: number): string => {
-        let repeatedValue: string = "";
-        while (times > 0) {
-            repeatedValue += value;
-            times--;
-        }
-        return repeatedValue;
-
-    }
-
-    private applyHeader = (rank: number) => {
-        const val: string = this.state.Value;
-        const start: number = this.state.Selected.Start;
-        this.setState(
-            {
-                Value: `${val.substring(0, start)}${this.repeatString("#", rank)} ${val.substring(start, val.length)}`,
-                Selected: { Start: start, End: val.length + rank + 1 },
-            },
-        );
-    }
-
-    private applyBold = () => {
-        const val: string = this.state.Value;
-        const start: number = this.state.Selected.Start;
-        const end: number = this.state.Selected.End;
-        this.setState(
-            {
-                Value: `${val.substring(0, start)}**${val.substring(start, end)}**${val.substring(end, val.length)}`,
-                Selected: { Start: start, End: end + 4 },
-            },
-        );
-    }
-
-    private applyItalic = () => {
-        const val: string = this.state.Value;
-        const start: number = this.state.Selected.Start;
-        const end: number = this.state.Selected.End;
-        this.setState(
-            {
-                Value: `${val.substring(0, start)}*${val.substring(start, end)}*${val.substring(end, val.length)}`,
-                Selected: { Start: start, End: end + 2 },
-            },
-        );
-    }
-
-    private applyStrikethrough = () => {
-        const val: string = this.state.Value;
-        const start: number = this.state.Selected.Start;
-        const end: number = this.state.Selected.End;
-        this.setState(
-            {
-                Value: `${val.substring(0, start)}~~${val.substring(start, end)}~~${val.substring(end, val.length)}`,
-                Selected: { Start: start, End: end + 4 },
-            },
-        );
-    }
-
-    private applyURL = () => {
-        const val: string = this.state.Value;
-        const start: number = this.state.Selected.Start;
-        const end: number = this.state.Selected.End;
-        this.setState(
-            {
-                Value: `${val.substring(0, start)}[${val.substring(start, end)}](url)${val.substring(end, val.length)}`,
-                Selected: { Start: start, End: end + 7 },
-            },
-        );
-    }
-
-    private applyBlockquote = () => {
-        const val: string = this.state.Value;
-        const start: number = this.state.Selected.Start;
-        this.setState(
-            {
-                Value: `${val.substring(0, start)}> ${val.substring(start, val.length)}`,
-                Selected: { Start: start, End: val.length + 2 },
-            },
         );
     }
 }
