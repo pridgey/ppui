08a40a8dc129c251520722c961332201234165af
Matt Pitcher <matt.e.pitcher@gmail.com>
2019-05-05 00:06:07 -0600

    correctly applies markdown for header, bold, itatlic, strike, url, quote

--- src/components/Inputs/MarkdownEditor/MarkdownEditor.tsx
+++ src/components/Inputs/MarkdownEditor/MarkdownEditor.tsx
@@ -1,20 +1,213 @@
 import * as React from "react";
-import styled from 'styled-components';
-import { theme } from "../../theming/theme";
+import styled from "styled-components";
+import { Button } from "../../";
+//import { theme } from "../../../theming/theme";
+
+const MDEWrapper = styled.div`
+    border: 1px solid pink;
+`;
+
+// use theme?
+const MarkdownBarWrapper = styled.div`
+    width: 100%;
+    background: gray;
+`;
+
+const MarkdownBar = styled.ul`
+    list-style: none;
+    display: flex;
+    margin: 0;
+    padding: 0;
+    font-size: 1em;
+    height: 100%;
+    align-items: center;
+`;
+
+const MarkdownItem = styled.li`
+    margin: 0 5px;
+`;
+
+const MarkdownButton = styled.button`
+    background: none;
+    border: none;
+    font: inherit;
+    cursor: pointer;
+`;
+
+const TextArea = styled.textarea`
+    height: 250px;
+    width: 100%;
+    padding: 10px;
+    border: none;
+    resize: none;
+    box-sizing: border-box;
+    font: inherit;
+`;
+
+interface ISelectionIndices {
+    Start: number;
+    End: number;
+}
 
 interface IMarkdownEditordProps {
-
+    Value?: string;
+    Placeholder?: string;
 }
 
 interface IMarkdownEditordState {
-
+    Value: string;
+    Selected: ISelectionIndices;
 }
 
 export class MarkdownEditor extends React.Component<IMarkdownEditordProps, IMarkdownEditordState> {
+    public constructor(props: IMarkdownEditordProps) {
+        super(props);
+        this.state = {
+            Value: this.props.Value || "",
+            Selected: {Start: 0, End: 0},
+        };
+    }
+
     public render() {
         return (
-            <textarea>
-            </textarea>
+            <MDEWrapper>
+                <MarkdownBarWrapper>
+                    <MarkdownBar>
+                        <MarkdownItem>
+                            <Button
+                                Caption="Write"
+                                OnClick={() => console.log("You clicked the write button")}
+                                Size="small"
+                                TextColor="Black" // use theme?
+                                ButtonColor="#6DD6FF" // use theme?
+                            />
+                            <Button
+                                Caption="Preview"
+                                OnClick={() => console.log("You clicked the preview button")}
+                                Size="small"
+                                TextColor="Black" // use theme?
+                                ButtonColor="#6DD6FF" // use theme?
+                            />
+                        </MarkdownItem>
+                        <MarkdownItem>
+                            <MarkdownButton onClick={() => this.applyHeader(1)}>H1</MarkdownButton>
+                        </MarkdownItem>
+                        <MarkdownItem>
+                            <MarkdownButton onClick={() => this.applyHeader(2)}>H2</MarkdownButton>
+                        </MarkdownItem>
+                        <MarkdownItem>
+                            <MarkdownButton onClick={this.applyBold}>B</MarkdownButton>
+                        </MarkdownItem>
+                        <MarkdownItem>
+                            <MarkdownButton onClick={this.applyItalic}>I</MarkdownButton>
+                        </MarkdownItem>
+                        <MarkdownItem>
+                            <MarkdownButton onClick={this.applyStrikethrough}>S</MarkdownButton>
+                        </MarkdownItem>
+                        <MarkdownItem>
+                            <MarkdownButton onClick={this.applyURL}>URL</MarkdownButton>
+                        </MarkdownItem>
+                        <MarkdownItem>
+                            <MarkdownButton onClick={this.applyBlockquote}>Quote</MarkdownButton>
+                        </MarkdownItem>
+                    </MarkdownBar>
+                </MarkdownBarWrapper>
+                <TextArea
+                    value={this.state.Value}
+                    placeholder={this.props.Placeholder}
+                    onChange={this.handleChange}
+                    onSelect={this.handleSelect}
+                />
+            </MDEWrapper>
+        );
+    }
+
+    private handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        this.setState({Value: event.target.value});
+    }
+
+    private handleSelect = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        const textArea: HTMLTextAreaElement = event.target;
+        this.setState({ Selected: { Start: textArea.selectionStart, End: textArea.selectionEnd }});
+    }
+
+    private repeatString = (value: string, times: number): string => {
+        let repeatedValue: string = "";
+        while (times > 0) {
+            repeatedValue += value;
+            times--;
+        }
+        return repeatedValue;
+
+    }
+
+    private applyHeader = (rank: number) => {
+        const val: string = this.state.Value;
+        const start: number = this.state.Selected.Start;
+        this.setState(
+            {
+                Value: `${val.substring(0, start)}${this.repeatString("#", rank)} ${val.substring(start, val.length)}`,
+                Selected: { Start: start, End: val.length + rank + 1 },
+            },
+        );
+    }
+
+    private applyBold = () => {
+        const val: string = this.state.Value;
+        const start: number = this.state.Selected.Start;
+        const end: number = this.state.Selected.End;
+        this.setState(
+            {
+                Value: `${val.substring(0, start)}**${val.substring(start, end)}**${val.substring(end, val.length)}`,
+                Selected: { Start: start, End: end + 4 },
+            },
+        );
+    }
+
+    private applyItalic = () => {
+        const val: string = this.state.Value;
+        const start: number = this.state.Selected.Start;
+        const end: number = this.state.Selected.End;
+        this.setState(
+            {
+                Value: `${val.substring(0, start)}*${val.substring(start, end)}*${val.substring(end, val.length)}`,
+                Selected: { Start: start, End: end + 2 },
+            },
+        );
+    }
+
+    private applyStrikethrough = () => {
+        const val: string = this.state.Value;
+        const start: number = this.state.Selected.Start;
+        const end: number = this.state.Selected.End;
+        this.setState(
+            {
+                Value: `${val.substring(0, start)}~~${val.substring(start, end)}~~${val.substring(end, val.length)}`,
+                Selected: { Start: start, End: end + 4 },
+            },
+        );
+    }
+
+    private applyURL = () => {
+        const val: string = this.state.Value;
+        const start: number = this.state.Selected.Start;
+        const end: number = this.state.Selected.End;
+        this.setState(
+            {
+                Value: `${val.substring(0, start)}[${val.substring(start, end)}](url)${val.substring(end, val.length)}`,
+                Selected: { Start: start, End: end + 7 },
+            },
+        );
+    }
+
+    private applyBlockquote = () => {
+        const val: string = this.state.Value;
+        const start: number = this.state.Selected.Start;
+        this.setState(
+            {
+                Value: `${val.substring(0, start)}> ${val.substring(start, val.length)}`,
+                Selected: { Start: start, End: val.length + 2 },
+            },
         );
     }
 }
